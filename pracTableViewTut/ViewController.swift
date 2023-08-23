ed92443dbbc08a206fe8ed6f1ad137c3063d2529
Carrington <Manyuchi>
2023-08-23 08:20:48 +0200

    Initial Commit

---
+++ pracTableViewTut/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pracTableViewTut
+//
+//  Created by DA MAC M1 157 on 2023/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
